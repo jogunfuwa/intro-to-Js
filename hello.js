1f1cfebf8877ac90e5eb82e49e87b0444f19608e
Julius Ogunfuwa <juliusogunfuwa@gmail.com>
2022-06-23 14:36:25 +0100

    task done

---
+++ hello.js
@@ -0,0 +1,11 @@
+// This prints out my name, height, and country.
+
+
+const about = {
+    name: "Julius Ogunfuwa",
+    country: "Nigeria",
+    height: 6.9
+};
+console.log(about.name);
+console.log(about.country);
+console.log(about.height);
